267f3475afad7d6e04c3a5390c68c4b6f3f45607
Angelo <angeloquispesuarez@gmail.com>
2025-07-15 23:41:01 -0500

    agregacion de dos estados

--- src/public/js/dashboardCli/components/funciones.js
+++ src/public/js/dashboardCli/components/funciones.js
@@ -1,82 +1,114 @@
   export let productos = [];
   export let pedidos_realizados = [];
   export let pedidos = [];
+  export let contador_pendientes = 0;
 
+
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>RENDERIZADOS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   export function cargarProductos(ruta) {
     return fetch(ruta)
-      .then(res => res.json())
-      .then(data => {
-        productos = data;
-        return data;
-      });
+    .then(res => res.json())
+    .then(data => {
+      productos = data;
+      return data;
+    });
   }
-
   export function cargarPedidos(ruta) {
     return fetch(ruta)
-      .then(res => res.json());
+    .then(res => res.json());
   }
-
+  
+  export function renderizarNumEstado(container) {
+    const despligue_contenedor = document.getElementById("circulo");
+    despligue_contenedor.innerHTML = `
+    <p>${contador_pendientes}</p>
+  `;
+}
 export function renderizarUltimoPedido(lista, container) {
   const ultimo = lista[lista.length - 1];
   const tabla = document.createElement("div");
-
+  
   const nombre = ultimo.items.map(i => i.name).join(", ");
-
+  
   tabla.innerHTML = `
-    <div class="last_name"><p>${nombre}</p></div>
-    <div class="duo_last">
-      <div class="last_date"><p>${ultimo.date}</p></div>
-      <div class="last_precio"><p>s/. ${ultimo.totalAmount.toFixed(2)}</p></div>
-    </div>
+  <div class="last_name"><p>${nombre}</p></div>
+  <div class="duo_last">
+  <div class="last_date"><p>${ultimo.date}</p></div>
+  <div class="last_precio"><p>s/. ${ultimo.totalAmount.toFixed(2)}</p></div>
+  </div>
   `;
-
+  
   container.appendChild(tabla);
 }
-
-
-export function renderizarListaPedidos(lista, container) {
+export function renderizarListaPedidos_std(lista, container) {
   container.innerHTML = "";
-
+  
   lista.forEach(element => {
     const nombres = element.items.map(i => i.name).join(", ");
     const cant = element.items.map(i => i.quantity).join(", ");
-
+    
     const tr = document.createElement("div");
     tr.className = "tabla";
     tr.innerHTML = `
-      <div>   
+    <div>   
+    <p>${nombres} (x${cant})</p>
+        <p>${element.date}</p>
+        </div>
+        <div class="std_right" style="margin-right:20px">
+        <p class="p_amarilla" id="text_std">Preparandose</p>  
+        <button class="bolita_amarilla"></button>
+        
+        </div>
+        `;
+        
+        const hr = document.createElement("hr");
+        
+        container.insertBefore(hr, container.firstChild);
+        container.insertBefore(tr, container.firstChild);
+      });
+    }
+    export function renderizarListaPedidos(lista, container) {
+      container.innerHTML = "";
+      
+      lista.forEach(element => {
+        const nombres = element.items.map(i => i.name).join(", ");
+        const cant = element.items.map(i => i.quantity).join(", ");
+        
+        const tr = document.createElement("div");
+        tr.className = "tabla";
+        tr.innerHTML = `
+        <div>   
         <p>${nombres} (x${cant})</p>
         <p>${element.date}</p>
-      </div>
-      <div><p>s/. ${element.totalAmount.toFixed(2)}</p></div>
-    `;
-
-    const hr = document.createElement("hr");
-
-    container.insertBefore(hr, container.firstChild);
-    container.insertBefore(tr, container.firstChild);
-  });
-}
-
-
-
-export function filtrar(texto, base, categoria = null) {
-  const t = texto.toLowerCase();
-
-  return base.filter(p => {
-    const coincideNombre = p.name.toLowerCase().includes(t);
-    const coincideCategoria = !categoria || p.category_id === categoria.category_id;
-    return coincideNombre && coincideCategoria;
-  });
-}
-
-
-export function mostrarResultados(text, array_filter, container, callbackAgregar) {
-  container.innerHTML = "";
-
-  if (text === "" || array_filter.length === 0) {
-    container.innerHTML = "<div class='result_negative'><p>Valores no encontrados</p></div>";
-    container.classList.remove("visible");
+        </div>
+        <div><p>s/. ${element.totalAmount.toFixed(2)}</p></div>
+        `;
+        
+        const hr = document.createElement("hr");
+        
+        container.insertBefore(hr, container.firstChild);
+        container.insertBefore(tr, container.firstChild);
+      });
+    }
+    
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>BUSQUEDA Y FILTRO>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    
+    
+    export function filtrar(texto, base, categoria = null) {
+      const t = texto.toLowerCase();
+      
+      return base.filter(p => {
+        const coincideNombre = p.name.toLowerCase().includes(t);
+        const coincideCategoria = !categoria || p.category_id === categoria.category_id;
+        return coincideNombre && coincideCategoria;
+      });
+    }
+    export function mostrarResultados(text, array_filter, container, callbackAgregar) {
+      container.innerHTML = "";
+      
+      if (text === "" || array_filter.length === 0) {
+        container.innerHTML = "<div class='result_negative'><p>Valores no encontrados</p></div>";
+        container.classList.remove("visible");
   } else {
     container.classList.add("visible");
     array_filter.forEach(element => {
@@ -131,8 +163,6 @@ export function mostrarResultados(text, array_filter, container, callbackAgregar
     });
   }
 }
-
-
 export function agregar(elemento, id, name_1, price, inputElement) {
   const cantidad = parseInt(inputElement.value);
   if (!cantidad || cantidad <= 0) return;
@@ -156,6 +186,8 @@ export function agregar(elemento, id, name_1, price, inputElement) {
   };
 
   pedidos_realizados.push(venta);
+  contador_pendientes++;
+
 
   // Mostrar en tabla visual
   const tbody = document.getElementById("tabla_pedidos");
@@ -188,6 +220,13 @@ export function agregar(elemento, id, name_1, price, inputElement) {
     });
   }
 
+
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FUNCIONES DE DEPLIEGUE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
 const toggleBtn = document.getElementById("toggleBtn");
 const contenido = document.getElementById("contenido");
 let visible = false;
@@ -200,6 +239,17 @@ function despligue() {
 toggleBtn.addEventListener("click", despligue);
 
 
+
+const toggleBtn_std = document.getElementById("toggleBtn_std");
+const contenido_std = document.getElementById("contenido_std");
+let visible_1 = false;
+function despligue_1(){
+  visible_1 = !visible_1;
+  contenido_std.style.display = visible_1? "block":"none";  
+}
+toggleBtn_std.addEventListener("click", despligue_1);
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 const toggleBtn_1 = document.getElementById("toggleBtn_factura");
 const factura = document.getElementById("factura");
 let visibble_factura = false ;
@@ -208,6 +258,7 @@ function despligue_factura(){
   factura.style.display = visibble_factura ? "flex " : "none";
 }
 toggleBtn_1.addEventListener("click", despligue_factura)
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 const toggleBtn_1_negative = document.getElementById("toggleBtn_factura_negative");
 const factura_negative = document.getElementById("factura");
@@ -216,6 +267,52 @@ function despligue_factura_negative(){
   factura_negative.style.display = visibble_factura_negative ? "none " : "flex";
 }
 toggleBtn_1_negative.addEventListener("click", despligue_factura_negative)
+
+
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>PARTE AGREGADA PA ALEJANDRO>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+export function consultarEstado() {
+  fetch('/estado')
+    .then(res => res.json())
+    .then(data => {
+      const div = document.getElementById("estado");
+
+      // Eliminar clases anteriores
+      div.classList.remove("no_pagado", "sirviendo", "listo_entregar");
+
+      // Agregar clase nueva
+      div.classList.add(data.estado);
+    })
+    .catch(err => console.error("Error al obtener el estado:", err));
+}
+
+export function actualizarEstadoBolita(boton, estado, element_P) {
+  boton.classList.remove("bolita_roja", "bolita_amarilla", "bolita_verde");
+
+  if (estado === "sirviendo") {
+    boton.classList.add("bolita_amarilla");
+    element_P.classList.add("p_amarilla")
+    element_P.innerHTML =`  
+    Preparando
+    `
+  } else if (estado === "listo_entregar") {
+    boton.classList.add("bolita_verde");
+    element_P.classList.add("p_verde");
+    element_P.innerHTML =`  
+    Listo para recoger
+    `;
+  }else if ( estado === "entragado") {
+    boton.classList.add("bolita_gray");
+    element_P.classList.add("p_gray");
+    element_P.innerHTML = `
+      Entregado
+    `
+    contador_pendientes--;
+  }
+
+}
 
 
 
--- src/public/js/dashboardCli/dashCli-script.js
+++ src/public/js/dashboardCli/dashCli-script.js
@@ -9,7 +9,9 @@ import {
   pedidos,
   productos,
   agregar,
-  enviarPedido
+  enviarPedido,
+  renderizarListaPedidos_std,
+  renderizarNumEstado,
 } from "/src/public/js/dashboardCli/components/funciones.js";
 
 
@@ -32,95 +34,84 @@ document.addEventListener("DOMContentLoaded", async () => {
   const search = document.getElementById("busqueda");
   const results = document.getElementById("resultados");
   const factura = document.getElementById("tabla_pedidos");
+  const std_pedidos = document.getElementById("contenido_std")
+
+  //FUNCION ENCARGADA DE CARGAR LOS PENDIENTES(ESTADOS DE CADA PEDIDO) 
+  //EN ESTE CASO 0 ---> MAS ADELANTE SE ACTUALIZA O SE VUEVE ACTUALIZAR
+  // EN LA FUNCION DE MOSTRAR PEDIDO SE SUMA CADA PENDIENTE
+  // SE RESTA EN LAFUCNION ED VER QUE ESTADO PASA ---> actualizarEstadoBolid
+  renderizarNumEstado();
     
   
   renderizarUltimoPedido(pedidosData, last_order);
   renderizarListaPedidos(pedidosData, list_order);
 
   //<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CARGAR CATEGORIA <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<{
-const productosData = await cargarProductos("/src/public/js/dashboardCli/productos.json");
-const categoriaSelect = document.getElementById("categoriaSelect");
-let categoriaSeleccionada = null;
+  const productosData = await cargarProductos("/src/public/js/dashboardCli/productos.json");
+  const categoriaSelect = document.getElementById("categoriaSelect");
+  let categoriaSeleccionada = null;
 
-//  Cargar y mostrar categorías en el select
-const categoriasData = await fetch("/src/public/js/dashboardCli/categorias.json").then(r => r.json());
-categoriasData.forEach(cat => {
-  const option = document.createElement("option");
-  option.value = cat.category_id;
-  option.textContent = cat.name;
-  categoriaSelect.appendChild(option);
-});
+  //  Cargar y mostrar categorías en el select
+  const categoriasData = await fetch("/src/public/js/dashboardCli/categorias.json").then(r => r.json());
+  categoriasData.forEach(cat => {
+    const option = document.createElement("option");
+    option.value = cat.category_id;
+    option.textContent = cat.name;
+    categoriaSelect.appendChild(option);
+  });
 
-//  Cuando se cambia la categoría
-categoriaSelect.addEventListener("change", () => {
-  const id = parseInt(categoriaSelect.value);
-  categoriaSeleccionada = categoriasData.find(c => c.category_id === id) || null;
-  const text = search.value.trim();
-  mostrarResultados(text, filtrar(text, productosData, categoriaSeleccionada), results, agregar);
-});
+  //  Cuando se cambia la categoría
+  categoriaSelect.addEventListener("change", () => {
+    const id = parseInt(categoriaSelect.value);
+    categoriaSeleccionada = categoriasData.find(c => c.category_id === id) || null;
+    const text = search.value.trim();
+    mostrarResultados(text, filtrar(text, productosData, categoriaSeleccionada), results, agregar);
+  });
 
-//  Búsqueda por texto + categoría
-search.addEventListener("input", debounce(() => {
-  const text = search.value.trim();
-  mostrarResultados(text, filtrar(text, productosData, categoriaSeleccionada), results, agregar);
-}, 300));
+  //  Búsqueda por texto + categoría
+  search.addEventListener("input", debounce(() => {
+    const text = search.value.trim();
+    mostrarResultados(text, filtrar(text, productosData, categoriaSeleccionada), results, agregar);
+  }, 300));
 
-search.addEventListener("focus", () => {
-  const text = search.value.trim();
-  mostrarResultados(text, filtrar(text, productosData, categoriaSeleccionada), results, agregar);
-});
-
-
-
-document.getElementById("btn-pagar").addEventListener("click", () => {
-  const modal = document.getElementById("modal");
-  modal.style.display = "flex";
-});
-
-document.getElementById("btn-confirmar").addEventListener("click", () => {
-  // Ocultar modal y factura
-  const modal = document.getElementById("modal");
-  modal.style.display = "none";
-  factura.style.display = "none";
-  
-  // Limpiar tabla visual
-  const tbody = document.getElementById("tabla_pedidos");
-  tbody.innerHTML = "";
-  
-  // Actualizar visual
-  last_order.innerHTML = "<h1>Ultimo pedido: </h1>";
-  const pedidosCompletos = [...pedidosData, ...pedidos_realizados];
-  renderizarUltimoPedido(pedidosCompletos, last_order);
-  renderizarListaPedidos(pedidosCompletos, list_order);
-  
-  // Enviar y limpiar datos
-  enviarPedido();
-  pedidos_realizados.length = 0;
-  pedidos.length = 0;
-  
-  alert("¡Pedido confirmado!");
-});
-
-
-});
+  search.addEventListener("focus", () => {
+    const text = search.value.trim();
+    mostrarResultados(text, filtrar(text, productosData, categoriaSeleccionada), results, agregar);
+  });
 
 
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  document.getElementById("btn-pagar").addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    modal.style.display = "flex";
+  });
 
-// search.addEventListener("input", debounce(() => {
-  //   const text = search.value.trim();
-  //   mostrarResultados(text, filtrar(text, productosData), results, agregar);
-  // }, 300)); 
-  
-  
-  // search.addEventListener("focus", () => {
-    //   const text = search.value.trim();
-    //   mostrarResultados(text, filtrar(text, productosData), results, agregar);
-    // });
+  document.getElementById("btn-confirmar").addEventListener("click", () => {
+    // Ocultar modal y factura
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+
     
-    // document.addEventListener("click", (e) => {
-      //   if (!results.contains(e.target) && !search.contains(e.target)) {
-        //     results.classList.remove("visible");
-        //   }
-        // });
+    // Limpiar tabla visual
+    const tbody = document.getElementById("tabla_pedidos");
+    tbody.innerHTML = "";
+    
+    // Actualizar visual
+    last_order.innerHTML = "<h1>Ultimo pedido: </h1>";
+    const pedidosCompletos = [...pedidosData, ...pedidos_realizados];
+    renderizarUltimoPedido(pedidosCompletos, last_order);
+    renderizarListaPedidos(pedidosCompletos, list_order);
+    renderizarListaPedidos_std(pedidos_realizados ,std_pedidos );
+
+    renderizarNumEstado();
+    
+    // Enviar y limpiar datos
+    enviarPedido();
+    pedidos_realizados.length = 0;
+    pedidos.length = 0;
+    
+    alert("¡Pedido confirmado!");
+  });
+
+
+});
